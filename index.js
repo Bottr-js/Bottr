de4ca5b5cf09daf6d07f657823a3d4cf9ea79759
James Campbell <james@supmenow.com>
2016-08-12 22:27:58 +0100

    Make router just a pure function.

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ var BotKit = require('./lib/botkit');
 
 var config = {
   client: new BotKit.CLIClient(),
-  router: {
-      route: function(bot, request, callback) {
+  router: function(bot, request, callback) {
 
-        var event = {
-          "request": request,
-          "message": request.message
-        }
-
-        var newContext = bot.triggerActionWithName("speak", event)
-        callback(newContext)
+      var event = {
+        "request": request,
+        "message": request.message
       }
+
+      var newContext = bot.triggerActionWithName("speak", event)
+      callback(newContext)
   },
   triggerActionWithName: function(name, event) {
       this.client.speak(event.message)
