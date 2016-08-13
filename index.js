53fb0ea49d5e4521435c65cce3d588b543801dad
James Campbell <james@supmenow.com>
2016-08-13 15:33:06 +0100

    Adds chat.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var BotKit = require('./lib/botkit');
 var config = {
   client: new BotKit.FacebookMessengerClient({
     appSecret: '770739fb5821713a8418148a16c352ef',
-    validationSecret: 'testing',
-    pageAccessToken: 'EAAQ4wdcZBuloBAHHhZCTMdodrpZBDpo5s8Y1JdEorCo6x5O3NUG7KG0vh7Hvz7GyZAWODwEs7cJKQeyVC5oTTBpc1PNtOF403tfCHnHLnWgKvQtfDWu1ZAu9gimsIAhQqpAbcz8IhH3K9jYMZBfoA6uerNfCHN75KxQSbKkFtKqwZDZD'
+    pageAccessToken: 'EAAQ4wdcZBuloBAHHhZCTMdodrpZBDpo5s8Y1JdEorCo6x5O3NUG7KG0vh7Hvz7GyZAWODwEs7cJKQeyVC5oTTBpc1PNtOF403tfCHnHLnWgKvQtfDWu1ZAu9gimsIAhQqpAbcz8IhH3K9jYMZBfoA6uerNfCHN75KxQSbKkFtKqwZDZD',
+    validationSecret: 'testing'
   }),
   router: function(bot, request, callback) {
 
