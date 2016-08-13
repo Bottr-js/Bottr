0ecb0267b0ee2a77e0ff2f63d3590d7e65bf32e1
James Campbell <james@supmenow.com>
2016-08-13 01:20:07 +0100

    Adds assertions for keys.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var BotKit = require('./lib/botkit');
 
 var config = {
-  client: new BotKit.FacebookMessengerClient(),
+  client: new BotKit.FacebookMessengerClient({
+    appSecret: '770739fb5821713a8418148a16c352ef',
+    validationSecret: 'testing',
+    pageAccessToken: 'EAAQ4wdcZBuloBAHHhZCTMdodrpZBDpo5s8Y1JdEorCo6x5O3NUG7KG0vh7Hvz7GyZAWODwEs7cJKQeyVC5oTTBpc1PNtOF403tfCHnHLnWgKvQtfDWu1ZAu9gimsIAhQqpAbcz8IhH3K9jYMZBfoA6uerNfCHN75KxQSbKkFtKqwZDZD'
+  }),
   router: function(bot, request, callback) {
 
       var action = {
