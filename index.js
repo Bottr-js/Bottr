fb17a9cdd3e44c4129e5baa79623af655320e161
James Campbell <james@supmenow.com>
2016-08-13 19:36:11 +0100

    Setting up word count.

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ var config = {
       }
 
       var context = Object.assign(defaults, request.context)
+      var messageCount = context.messageCount + 1;
+      var wordCount = context.wordCount + request.message.split(" ").length;
 
       switch (action.type) {
         case "speak":
@@ -31,14 +33,14 @@ var config = {
         break
 
         case "stats":
-        this.client.speak("Total Message Count: " + context.messageCount, request.user)
-        this.client.speak("Total Word Count: " + context.wordCount, request.user)
+        this.client.speak("Total Message Count: " + messageCount, request.user)
+        this.client.speak("Total Word Count: " + wordCount, request.user)
         break
       }
 
       return Object.assign(request.context, {
-        messageCount: 0,
-        wordCount: 0
+        messageCount: messageCount,
+        wordCount: wordCount
       })
   }
 };
