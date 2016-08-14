b22c57e2cc85675f506b05511d6401b1cfd98c37
James Campbell <james@supmenow.com>
2016-08-14 18:40:55 +0100

    First test.

---
+++ __tests__/bot.js
@@ -0,0 +1,10 @@
+jest.unmock('../lib/bot');
+
+describe('bot', () => {
+  it('throws error when no configuration is provided', () => {
+
+    const Bot = require('../lib/bot');
+
+    expect(() => new Bot() ).toThrow();
+  });
+});
