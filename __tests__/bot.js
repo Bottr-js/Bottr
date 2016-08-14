40ef2b4ce7168939bd2dbb61225f8f11a4606148
James Campbell <james@supmenow.com>
2016-08-14 18:44:29 +0100

    All construction tests built.

--- __tests__/bot.js
+++ __tests__/bot.js
@@ -5,4 +5,18 @@ describe('bot', () => {
     const Bot = require('../lib/bot');
     expect(() => new Bot() ).toThrow();
   });
+
+  it('throws error when no client is provided', () => {
+    const Bot = require('../lib/bot');
+    expect(() => new Bot({
+      router: () => {}
+    }) ).toThrow();
+  });
+
+  it('throws error when no router is provided', () => {
+    const Bot = require('../lib/bot');
+    expect(() => new Bot({
+      client: {}
+    })).toThrow();
+  });
 });
