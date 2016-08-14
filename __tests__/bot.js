4ffab60fe8c3b696363baadafff4c8176bcea15a
James Campbell <james@supmenow.com>
2016-08-14 18:41:17 +0100

    Removes spacing.

--- __tests__/bot.js
+++ __tests__/bot.js
@@ -2,9 +2,7 @@ jest.unmock('../lib/bot');
 
 describe('bot', () => {
   it('throws error when no configuration is provided', () => {
-
     const Bot = require('../lib/bot');
-
     expect(() => new Bot() ).toThrow();
   });
 });
