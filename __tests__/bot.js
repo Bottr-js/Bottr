8fcbebc0807652e92ebf72d905ae75a21c4a7c96
James Campbell <james@supmenow.com>
2016-08-14 18:45:36 +0100

    Test.

--- __tests__/bot.js
+++ __tests__/bot.js
@@ -1,20 +1,19 @@
 jest.unmock('../lib/bot');
 
+const Bot = require('../lib/bot');
+
 describe('bot', () => {
   it('throws error when no configuration is provided', () => {
-    const Bot = require('../lib/bot');
     expect(() => new Bot() ).toThrow();
   });
 
   it('throws error when no client is provided', () => {
-    const Bot = require('../lib/bot');
     expect(() => new Bot({
       router: () => {}
     }) ).toThrow();
   });
 
   it('throws error when no router is provided', () => {
-    const Bot = require('../lib/bot');
     expect(() => new Bot({
       client: {}
     })).toThrow();
