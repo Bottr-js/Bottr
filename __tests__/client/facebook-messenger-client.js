af75b5b80736cdbd966c0ba7c7169f61626e9c3c
James Campbell <james@supmenow.com>
2016-08-15 13:44:29 +0100

    Configuration test passes.

--- __tests__/client/facebook-messenger-client.js
+++ __tests__/client/facebook-messenger-client.js
@@ -1,7 +1,7 @@
 jest.unmock('../../lib/client/facebook-messenger-client');
 
-jest.setMock('messenger-bot', function() {
-  
+jest.setMock('messenger-bot', function(config) {
+  this.config = config
 })
 
 const FacebookMessengerClient = require('../../lib/client/facebook-messenger-client');
@@ -23,5 +23,20 @@ describe('facebook messenger client', () => {
       pageAccessToken: 'Token',
       validationSecret: 'Secret'
     })).toThrow();
+  });
+
+  it('constructs messenger bot with configuration options', () => {
+
+    var options = {
+      pageAccessToken: 'Token',
+      validationSecret: 'Secret',
+      appSecret: 'Secret'
+    }
+
+    var client = new FacebookMessengerClient(options)
+
+    expect(client.messenger.config.token).toEqual(options.pageAccessToken)
+    expect(client.messenger.config.verify).toEqual(options.validationSecret)
+    expect(client.messenger.config.app_secret).toEqual(options.appSecret)
   });
 });
