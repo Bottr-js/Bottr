f123e2517b43d911f0937386ca376098ea2a1896
James Campbell <james@supmenow.com>
2016-08-15 13:04:14 +0100

    Adds tests for memory context store.

---
+++ __tests__/context-store/memory-context-store.js
@@ -0,0 +1,7 @@
+jest.unmock('../lib/context-store/memory-context-store.js');
+
+const MemoryContextStore = require('../lib/context-store/memory-context-store.js');
+
+describe('memory context store', () => {
+  
+});
