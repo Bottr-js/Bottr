bc41f3badc5cbf15c4896e5c3c044cf31e15b77d
James Campbell <james@supmenow.com>
2016-08-15 13:08:20 +0100

    Preparing for memeory context tests.

--- __tests__/context-store/memory-context-store.js
+++ __tests__/context-store/memory-context-store.js
@@ -1,7 +1,26 @@
-jest.unmock('../lib/context-store/memory-context-store.js');
+jest.unmock('../../lib/context-store/memory-context-store.js');
 
-const MemoryContextStore = require('../lib/context-store/memory-context-store.js');
+const MemoryContextStore = require('../../lib/context-store/memory-context-store.js');
 
 describe('memory context store', () => {
-  
+
+  it('calls callback once constructed', () => {
+    var callback = jest.fn()
+    var contextStore = new MemoryContextStore(callback)
+    expect(callback).toBeCalled()
+  })
+
+  it('stores context', () => {
+    var contextStore = new MemoryContextStore(jest.fn())
+    contextStore.set("key", {})
+    expect(contextStore.memory["key"]).toBeTruthy()
+  });
+
+  it('retrieves context', () => {
+    var contextStore = new MemoryContextStore(jest.fn())
+    contextStore.memory = {"key": {}}
+    contextStore.get("key", (context) => {
+      expect(context).toBeTruthy()
+    })
+  });
 });
