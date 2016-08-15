c6ee2f4a85c31256ec54e645d8e4a726d9c37ef9
James Campbell <james@supmenow.com>
2016-08-15 13:41:42 +0100

    Initial tests for facebook client pass.

--- __tests__/client/facebook-messenger-client.js
+++ __tests__/client/facebook-messenger-client.js
@@ -1,8 +1,27 @@
 jest.unmock('../../lib/client/facebook-messenger-client');
 
+jest.setMock('messenger-bot', function() {
+  
+})
+
 const FacebookMessengerClient = require('../../lib/client/facebook-messenger-client');
 
 describe('facebook messenger client', () => {
 
-  
+  it('throws error when no page access token is provided', () => {
+    expect(() => new FacebookMessengerClient()).toThrow();
+  });
+
+  it('throws error when no validation secret is provided', () => {
+    expect(() => new FacebookMessengerClient({
+      pageAccessToken: 'Token'
+    })).toThrow();
+  });
+
+  it('throws error when no app secret is provided', () => {
+    expect(() => new FacebookMessengerClient({
+      pageAccessToken: 'Token',
+      validationSecret: 'Secret'
+    })).toThrow();
+  });
 });
