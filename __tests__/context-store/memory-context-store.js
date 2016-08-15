76a910aa511da5dd28b0509bc27db126dbb39f09
James Campbell <james@supmenow.com>
2016-08-15 13:09:28 +0100

    Memory context store fully tested.

--- __tests__/context-store/memory-context-store.js
+++ __tests__/context-store/memory-context-store.js
@@ -4,20 +4,23 @@ const MemoryContextStore = require('../../lib/context-store/memory-context-store
 
 describe('memory context store', () => {
 
-  it('calls callback once constructed', () => {
+  it('calls callback once started', () => {
     var callback = jest.fn()
-    var contextStore = new MemoryContextStore(callback)
+
+    var contextStore = new MemoryContextStore()
+    contextStore.start(callback)
+
     expect(callback).toBeCalled()
   })
 
   it('stores context', () => {
-    var contextStore = new MemoryContextStore(jest.fn())
+    var contextStore = new MemoryContextStore()
     contextStore.set("key", {})
     expect(contextStore.memory["key"]).toBeTruthy()
   });
 
   it('retrieves context', () => {
-    var contextStore = new MemoryContextStore(jest.fn())
+    var contextStore = new MemoryContextStore()
     contextStore.memory = {"key": {}}
     contextStore.get("key", (context) => {
       expect(context).toBeTruthy()
