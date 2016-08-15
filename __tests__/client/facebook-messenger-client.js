65aa7d8b04f5647651c32a6f2a398620197aed6e
James Campbell <james@supmenow.com>
2016-08-15 13:38:29 +0100

    Preparing for facebook messenger tests.

---
+++ __tests__/client/facebook-messenger-client.js
@@ -0,0 +1,8 @@
+jest.unmock('../../lib/client/facebook-messenger-client');
+
+const FacebookMessengerClient = require('../../lib/client/facebook-messenger-client');
+
+describe('facebook messenger client', () => {
+
+  
+});
