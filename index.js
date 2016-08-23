eca7e284266e0c8a2a3cd75a7b416793dd2e3f68
James Campbell <james@supmenow.com>
2016-08-23 15:05:42 +0100

    Prepare contect.

--- index.js
+++ index.js
@@ -80,57 +80,58 @@ function HubotBot(hubot) {
   return hubot_botkit
 }
 
-function Neurotin(bot) {
+function Neurotin(controller) {
 
-  bot.on('message_received', function() {
-    console.log('fuck')
+  controller.on('message_received', function(bot, message) {
 
-    //
-    //   // Default values for if this is the first time
-    //   // communicating with the bot
-    //   var defaults = {
-    //     messageCount: 0,
-    //     wordCount: 0
-    //   }
-    //
-    //   // Merge current context with the defaults.
-    //   var context = Object.assign({}, defaults, request.context)
-    //
-    //   // Increment the message count and
-    //   // calculate the new number of words the user has sent
-    //   var messageCount = context.messageCount + 1;
-    //   var wordCount = context.wordCount + request.message.split(" ").length;
-    //
-    //   // Merge the new statistics into the context
-    //   // and return it back to the bot
-    //   return Object.assign({}, context, {
-    //     messageCount: messageCount,
-    //     wordCount: wordCount
-    //   })
+    controller.storage.users.get(message.user, function(err, context) {
+        // Default values for if this is the first time
+        // communicating with the bot
+        var defaults = {
+          messageCount: 0,
+          wordCount: 0
+        }
+
+        // Merge current context with the defaults.
+        var context = Object.assign({}, defaults, context)
+
+        // Increment the message count and
+        // calculate the new number of words the user has sent
+        //
+        // FIXME: Why do we need to text.text on an event but not a listener
+        var messageCount = context.messageCount + 1;
+        var wordCount = context.wordCount + message.text.text.split(" ").length;
+
+        // Merge the new statistics into the context
+        // and return it back to the bot
+        controller.storage.users.save({
+          id: message.user,
+          context: Object.assign({}, context, {
+            messageCount: messageCount,
+            wordCount: wordCount
+          })
+        }, function(){});
+    });
   })
 
-  bot.hears(['\/stats'], ['message_received'], function(bot, message) {
-      bot.reply(message, 'Stats would be calculated here');
+  controller.hears(['\/stats'], ['message_received'], function(bot, message) {
+      controller.storage.users.get(message.user, function(err, context) {
+        // Send the total number of messages to the
+        // user
+        bot.reply(message, "Total Message Count: " + context.messageCount)
 
-      //
-      //   // Send the total number of messages to the
-      //   // user
-      //   bot.speak({
-      //     message: "Total Message Count: " + request.context.messageCount
-      //   }, request)
-      //
-      //   // Send the total number of words to the
-      //   // user
-      //   bot.speak({
-      //     message: "Total Word Count: " + request.context.wordCount
-      //   }, request)
-      //
-      //   // We don't do anything to the context
-      //   // so we just return it
-      //   return request.context
+        // Send the total number of words to the
+        // user
+        bot.speak(message, "Total Word Count: " +context.wordCount)
+
+        //
+        // We don't do anything to the context
+        // so we just return it
+        //return request.context
+      });
   });
 
-  bot.hears([/.+/], ['message_received'], function(bot, message) {
+  controller.hears([/.+/], ['message_received'], function(bot, message) {
       bot.reply(message, message.text);
   });
 }
