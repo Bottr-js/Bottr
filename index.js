27b6a4a58981ed257e599c5fa9f3cd45c4cd337d
James Campbell <james@supmenow.com>
2016-08-26 08:45:32 +0100

    Tweaks for the future.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib')
+var Pozi = require('./lib')
+
+function Client() {
+
+}
+
+var bot = new Pozi.Bot2()
+
+// bot.use(<component>)
+
+// bot.on(<event>, <cb>)
+
+// bot.hears(<pattern>, <cb>)
+
+bot.listen(3000)
+
+//module.exports = require('./lib')
