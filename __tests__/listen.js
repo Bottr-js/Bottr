189e070ae9b29048ce056f458f4409417e51b63f
James Campbell <james@supmenow.com>
2016-09-05 16:29:53 +0100

    Test as port.

--- __tests__/listen.js
+++ __tests__/listen.js
@@ -8,21 +8,30 @@ require('../lib/listen')
 test('listen returns server', () => {
   var bot = new Bot()
   var server = bot.listen()
-  expect(server).not.toBeNull();
+  expect(server).not.toBeNull()
 });
 
 test('listen configures server with bot', () => {
-  //   server.use(this)
+  var bot = new Bot()
+  var server = bot.listen()
+  expect(server.use).toBeCalledWith(bot)
 });
 
-// port
-//   var serverPort = port || process.env.PORT || 3000
-//   server.listen(serverPort)
+test('listen defaults to port 3000', () => {
+  var bot = new Bot()
+  var server = bot.listen()
+  expect(server.listen).toBeCalledWith(3000)
+});
 
-// env
-//   var serverPort = port || process.env.PORT || 3000
-//   server.listen(serverPort)
+test('listen uses port from enviroment', () => {
+  var bot = new Bot()
+  process.env.PORT = 2000
+  var server = bot.listen()
+  expect(server.listen).toBeCalledWith('2000')
+});
 
-// default
-//   var serverPort = port || process.env.PORT || 3000
-//   server.listen(serverPort)
+test('listen uses port passed in as argument', () => {
+  var bot = new Bot()
+  var server = bot.listen(1000)
+  expect(server.listen).toBeCalledWith(1000)
+});
