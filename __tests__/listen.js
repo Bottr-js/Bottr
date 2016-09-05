07bdfadee076455dc27f047a91687cd01c04c37c
James Campbell <james@supmenow.com>
2016-09-05 16:18:52 +0100

    Test server.

--- __tests__/listen.js
+++ __tests__/listen.js
@@ -1,3 +1,20 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+jest.unmock('../lib/bot')
+jest.unmock('../lib/listen')
+
+var Bot = require('../lib/bot')
+
+require('../lib/listen')
+
+test('returns server', () => {
+  var bot = new Bot()
+  var server = bot.listen()
+  expect(server).not.toBeNull();
 });
+
+// Bot.prototype.listen = function(port) {
+//   var serverPort = port || process.env.PORT || 3000
+//   var server = new Server()
+//   server.use(this)
+//   server.listen(serverPort)
+//   console.log('Bot is listening on port ' + serverPort)
+// }
