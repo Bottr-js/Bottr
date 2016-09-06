5c70d38a91afde02bde41abbadd563c286dfc315
James Campbell <james@supmenow.com>
2016-09-06 10:38:06 +0100

    Implemented magic.

--- __tests__/event-emitter.js
+++ __tests__/event-emitter.js
@@ -37,12 +37,6 @@ test('should register fallback', () => {
 });
 
 // EventEmitter.prototype.emit = function(eventName) {
-//   var emitter = this
-//   var args = Array.prototype.concat.apply([], arguments).slice(1)
-//   var eventListeners = this.listeners.filter(function(listener) {
-//     return listener.eventName === eventName
-//   })
-//
 //   var event = new Event(eventName, eventListeners, args)
 //   event.next(function(eventName) {
 //
@@ -56,3 +50,54 @@ test('should register fallback', () => {
 //   })
 // }
 //
+
+test('should emit event', () => {
+  var emitter = new EventEmitter()
+  var handler = jest.fn()
+
+  emitter.addListener('event', handler)
+  var event = emitter.emit('event')
+
+  expect(event.eventName).toEqual('event')
+});
+
+test('should emit event with correct handlers', () => {
+  var emitter = new EventEmitter()
+  var handler = jest.fn()
+  var handler2 = jest.fn()
+
+  emitter.addListener('event', handler)
+  emitter.addListener('event2', handler2)
+  var event = emitter.emit('event')
+
+  expect(event.remainingListeners).toEqual([handler])
+});
+
+test('should emit event with correct arguments', () => {
+  var emitter = new EventEmitter()
+  var handler = jest.fn()
+
+  emitter.addListener('event', handler)
+  var event = emitter.emit('event', 1)
+
+  expect(event.args).toEqual([
+    1,
+    jest.fn()
+  ])
+});
+//
+// test('should use fallback for event with no handlers', () => {
+//   var emitter = new EventEmitter()
+//   var handler = jest.fn()
+//
+//   emitter.fallback('event', handler)
+//   expect(emitter.fallbacks['event']).toEqual(handler)
+// });
+//
+// test('should log error for event with no handlers or fallback', () => {
+//   var emitter = new EventEmitter()
+//   var handler = jest.fn()
+//
+//   emitter.fallback('event', handler)
+//   expect(emitter.fallbacks['event']).toEqual(handler)
+// });
