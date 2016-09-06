e2c858626e93f23da961be46ea76b8b25506452b
James Campbell <james@supmenow.com>
2016-09-06 09:54:03 +0100

    Implemented response middleware tests.

--- __tests__/response-middleware.js
+++ __tests__/response-middleware.js
@@ -1,3 +1,36 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+jest.unmock('../lib/response-middleware')
+
+var ResponseMiddleware = require('../lib/response-middleware')
+
+var res = {
+  sendStatus: jest.fn(),
+  status: jest.fn(),
+  json: jest.fn()
+}
+
+var req = {}
+var next = jest.fn()
+
+var middleware = new ResponseMiddleware()
+middleware(req, res, next)
+
+test('should send status code 200 on success', () => {
+  res.success()
+  expect(res.sendStatus).toBeCalledWith(200)
+});
+
+test('should send status code 400 on error', () => {
+  res.error()
+  expect(res.status).toBeCalledWith(400)
+});
+
+test('should send message on error', () => {
+  res.error('Error')
+  expect(res.json).toBeCalledWith({
+    error: 'Error'
+  })
+});
+
+test('should call next', () => {
+  expect(next).toBeCalled()
 });
