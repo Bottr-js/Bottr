de02a9db6c1c3e8777851b9d06216df7fcd64869
James Campbell <james@supmenow.com>
2016-09-06 11:22:21 +0100

    Implement event unit tests.

--- __tests__/event.js
+++ __tests__/event.js
@@ -1,3 +1,33 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+jest.unmock('../lib/event')
+
+var Event = require('../lib/event')
+
+test('should call handler', () => {
+  var handler = jest.fn()
+  var event = new Event([handler], [])
+
+  event.next(jest.fn())
+
+  expect(handler).toBeCalled();
+});
+
+test('should call next handler when previous handler doesnt handle event', () => {
+  var handler = function(next) {
+    next()
+  }
+  var handler2 = jest.fn()
+  var event = new Event([handler, handler2], [])
+
+  event.next(jest.fn())
+
+  expect(handler2).toBeCalled();
+});
+
+test('should call unhandled callback if no handler handles event', () => {
+  var callback = jest.fn()
+  var event = new Event([], [])
+
+  event.next(callback)
+
+  expect(callback).toBeCalled();
 });
