ba9e7e0315ddb46d596641f5da30a07cef582859
James Campbell <james@supmenow.com>
2016-09-06 12:36:35 +0100

    Testing connection.

--- __tests__/websocket-client.js
+++ __tests__/websocket-client.js
@@ -3,16 +3,45 @@ jest.unmock('../lib/websocket-client')
 var io = require('socket.io')
 var WebsocketClient = require('../lib/websocket-client')
 
+var socketClient = io()
 var session = {
   socket: {
     emit: jest.fn()
   }
 }
 
-var client = new WebsocketClient(io())
+// WebsocketClient.prototype.onConnection = function(socket) {
+//     console.log('new websocket connection')
+//
+//     socket.on('message', function(data) {
+//
+//       var session = new Session(data.user, {}, this)
+//       session.socket = socket
+//
+//       bot.trigger('message_received', data, session)
+//
+//     }.bind(this))
+// }
+
+test('should handle web socket connection event', function() {
+
+  var client = new WebsocketClient(socketClient)
+  client.onConnection = jest.fn()
+
+  expect(socketClient.on).toBeCalledWith('connection', client.onConnection)
+})
+//
+// test('should emit typing event with text when sending typing event', function() {
+//
+//   client.startTyping(session)
+//
+//   expect(session.socket.emit).toBeCalledWith('typing', {})
+// })
+
 
 test('should emit message event with text when sending message', function() {
 
+  var client = new WebsocketClient(socketClient)
   client.send(session, 'Hey')
 
   expect(session.socket.emit).toBeCalledWith('message', {
@@ -22,38 +51,8 @@ test('should emit message event with text when sending message', function() {
 
 test('should emit typing event with text when sending typing event', function() {
 
+  var client = new WebsocketClient(socketClient)
   client.startTyping(session)
 
   expect(session.socket.emit).toBeCalledWith('typing', {})
 })
-
-// test('should emit typing event with text when sending typing event', function() {
-//
-//   client.startTyping(session)
-//
-//   expect(session.socket.emit).toBeCalledWith('typing', {})
-// })
-
-// function WebsocketClient(io) {
-//
-//   var client = this
-//
-//   return function(bot) {
-//
-//     var bot = this
-//
-//     io.on('connection',  function (socket) {
-//
-//       console.debug('new websocket connection')
-//
-//       socket.on('message', function(data) {
-//
-//         var session = new Session(data.user, {}, client)
-//         session.socket = socket
-//
-//         bot.trigger('message_received', data, session)
-//       })
-//     });
-//   }
-// }
-//
