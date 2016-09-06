82968a5f360bf0901111c32596b295cf8af07230
James Campbell <james@supmenow.com>
2016-09-06 16:39:10 +0100

    All config tests passed.

--- __tests__/facebook-messenger-client.js
+++ __tests__/facebook-messenger-client.js
@@ -5,8 +5,26 @@ var FacebookMessengerClient = require('../lib/facebook-messenger-client')
 
 var bot = new Bot()
 
-test('adds 1 + 2 to equal 3', () => {
+test('should use enviromental variables for access and verify token', () => {
+
+  process.env.MESSENGER_ACCESS_TOKEN = 'access'
+  process.env.MESSENGER_VERIFY_TOKEN = 'verify'
 
   var client = new FacebookMessengerClient(bot)
 
+  expect(client.config.access_token).toEqual('access')
+  expect(client.config.verify_token).toEqual('verify')
+  process.env.MESSENGER_ACCESS_TOKEN = undefined
+  process.env.MESSENGER_VERIFY_TOKEN = undefined
+});
+
+test('should use configuration object for access and verify token', () => {
+
+  var client = new FacebookMessengerClient(bot, {
+    access_token: 'access',
+    verify_token: 'verify'
+  })
+
+  expect(client.config.access_token).toEqual('access')
+  expect(client.config.verify_token).toEqual('verify')
 });
