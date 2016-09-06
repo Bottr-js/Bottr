7de2de1e6d4dffba576ad3e87c2ac577faaa0fcb
James Campbell <james@supmenow.com>
2016-09-06 16:27:37 +0100

    Preparing for facebook tests.

--- __tests__/facebook-messenger-client.js
+++ __tests__/facebook-messenger-client.js
@@ -1,3 +1,7 @@
+jest.unmock('../lib/facebook-messenger-client')
+
+var FacebookMessengerClient = require('../lib/facebook-messenger-client')
+
 test('adds 1 + 2 to equal 3', () => {
   expect(3).toBe(3);
 });
