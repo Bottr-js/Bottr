4baf16b3acf7a5bb745335584b4ac88e5ab0a6c6
James Campbell <james@supmenow.com>
2016-09-06 16:33:26 +0100

    Fixed error.

--- __tests__/facebook-messenger-client.js
+++ __tests__/facebook-messenger-client.js
@@ -1,7 +1,12 @@
 jest.unmock('../lib/facebook-messenger-client')
 
+var Bot = require('../lib/bot')
 var FacebookMessengerClient = require('../lib/facebook-messenger-client')
 
+var bot = new Bot()
+
 test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+
+  var client = new FacebookMessengerClient(bot)
+
 });
