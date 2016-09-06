75e5ba727a366ea0fd96b1f0d2001f4d66988991
James Campbell <james@supmenow.com>
2016-09-06 16:41:04 +0100

    REmoves olf fixme.

--- __tests__/facebook-messenger-client.js
+++ __tests__/facebook-messenger-client.js
@@ -28,3 +28,117 @@ test('should use configuration object for access and verify token', () => {
   expect(client.config.access_token).toEqual('access')
   expect(client.config.verify_token).toEqual('verify')
 });
+// 
+// bot.on('webhook', function(req, res, next) {
+//
+//   // If this isn't a facebook request then carry on with other handlers
+//   if ( !req.headers.hasOwnProperty('x-hub-signature') && req.headers['user-agent'].indexOf('facebookplatform') === -1) {
+//     next()
+//     return
+//   }
+//
+//   var query = req.query
+//
+//   if ( query['hub.mode'] === 'subscribe') {
+//     client.handleSubscription(req, res)
+//   } else {
+//     client.handleEvent(bot, req, res)
+//   }
+//
+//   res.success()
+// }.bind(this))
+// }
+//
+// // FIXME: Allow token to be configured
+// FacebookMessengerClient.prototype.handleSubscription = function(req, res) {
+//
+// var query = req.query
+//
+// if (query['hub.verify_token'] === this.config.verify_token) {
+//   res.send(query['hub.challenge'])
+// } else {
+//   res.sendStatus(403)
+// }
+// }
+//
+// FacebookMessengerClient.prototype.handleEvent = function(bot, req, res) {
+//
+// var client = this
+// var body = req.body
+//
+// if (body.object == 'page') {
+//
+//   body.entry.forEach(function(pageEntry) {
+//
+//     pageEntry.messaging.forEach(function(messagingEvent) {
+//       if (messagingEvent.message) {
+//         client.receivedMessage(bot, messagingEvent);
+//       } else {
+//         console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+//       }
+//     });
+//
+//   });
+//
+// }
+// }
+//
+// FacebookMessengerClient.prototype.receivedMessage = function(bot, event) {
+// var senderID = event.sender.id
+// var message = event.message
+//
+// var session = new Session(senderID, {}, this)
+//
+// bot.trigger('message_received', message, session)
+// }
+//
+// FacebookMessengerClient.prototype.send = function(session, text) {
+//
+// console.log('Sending "' + text + '"')
+//
+// var messageData = {
+//   recipient: {
+//     id: session.user
+//   },
+//   message: {
+//     text: text
+//   }
+// };
+//
+// request({
+//   uri: 'https://graph.facebook.com/v2.6/me/messages',
+//   qs: { access_token: this.config.access_token },
+//   method: 'POST',
+//   json: messageData
+//
+// }, function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log("Successfully sent message.");
+//   } else {
+//     console.error("Unable to send message.");
+//   }
+// });
+// }
+//
+// FacebookMessengerClient.prototype.startTyping = function(session) {
+//
+// var messageData = {
+//   recipient: {
+//     id: session.user
+//   },
+//   sender_action: "typing_on"
+// }
+//
+// request({
+//   uri: 'https://graph.facebook.com/v2.6/me/messages',
+//   qs: { access_token: this.config.access_token },
+//   method: 'POST',
+//   json: messageData
+// }, function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log("Successfully sent message.");
+//   } else {
+//     console.error("Unable to send message.");
+//   }
+// });
+// }
