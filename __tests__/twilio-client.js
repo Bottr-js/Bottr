9859a94ab70b7107c8792108dc787a33ba0029cf
James Campbell <james@supmenow.com>
2016-09-07 16:31:28 +0100

    Remove config for twilio.

--- __tests__/twilio-client.js
+++ __tests__/twilio-client.js
@@ -1,16 +1,11 @@
 jest.unmock('../lib/twilio-client')
 
 var Bot = require('../lib/bot')
-var TwillioClient = require('../lib/twilio-client')
+var TwilioClient = require('../lib/twilio-client')
 
 var bot = new Bot()
 
-//   var defaults = {
-//     account_sid: process.env.TWILIO_ACCOUNT_SID,
-//     auth_token: process.env.TWILIO_AUTH_TOKEN,
-//     phone_number: process.env.TWILIO_PHONE_NUMBER
-//   }
-//
+
 //
 //     bot.on('webhook', function(req, res, next) {
 //
@@ -30,48 +25,50 @@ var bot = new Bot()
 //
 //       bot.trigger('message_received', message, session)
 //       res.send({}) // We can't send a success code as twillio will send it
-//     })
+//     })]
 
-// test('should use enviromental variables for key, secrets and tokens', () => {
-//
-//   process.env.TWITTER_CONSUMER_KEY = 'key'
-//   process.env.TWITTER_CONSUMER_SECRET = 'secret'
-//   process.env.TWITTER_ACCESS_TOKEN = 'token'
-//   process.env.TWITTER_ACCESS_TOKEN_SECRET = 'secret'
-//
-//   var client = new TwitterClient(bot)
-//
-//   expect(client.config.consumer_key).toEqual('key')
-//   expect(client.config.consumer_secret).toEqual('secret')
-//   expect(client.config.access_token).toEqual('token')
-//   expect(client.config.access_token_secret).toEqual('secret')
-//
-//   process.env.TWITTER_CONSUMER_KEY = undefined
-//   process.env.TWITTER_CONSUMER_SECRET = undefined
-//   process.env.TWITTER_ACCESS_TOKEN = undefined
-//   process.env.TWITTER_ACCESS_TOKEN_SECRET = undefined
-// });
-//
-// test('should use configuration object for key, secrets and tokens', () => {
-//
-//   var config = {
-//     consumer_key: 'key',
-//     consumer_secret: 'secret',
-//     access_token: 'token',
-//     access_token_secret: 'secret'
+
+//   var defaults = {
+//     account_sid: process.env.TWILIO_ACCOUNT_SID,
+//     auth_token: process.env.TWILIO_AUTH_TOKEN,
+//     phone_number: process.env.TWILIO_PHONE_NUMBER
 //   }
-//
-//   var client = new TwitterClient(bot, config)
-//
-//   expect(client.config.consumer_key).toEqual('key')
-//   expect(client.config.consumer_secret).toEqual('secret')
-//   expect(client.config.access_token).toEqual('token')
-//   expect(client.config.access_token_secret).toEqual('secret')
-// });
+
+test('should use enviromental variables for sid, token and phone number', () => {
+
+  process.env.TWILIO_ACCOUNT_SID = 'sid'
+  process.env.TWILIO_AUTH_TOKEN = 'token'
+  process.env.TWILIO_PHONE_NUMBER = 'number'
+
+  var client = new TwilioClient(bot)
+
+  expect(client.config.account_sid).toEqual('sid')
+  expect(client.config.auth_token).toEqual('token')
+  expect(client.config.phone_number).toEqual('number')
+
+  process.env.TWILIO_ACCOUNT_SID = undefined
+  process.env.TWILIO_AUTH_TOKEN = undefined
+  process.env.TWILIO_PHONE_NUMBER = undefined
+});
+
+test('should use configuration object for key, secrets and tokens', () => {
+
+  var config = {
+    account_sid: 'sid',
+    auth_token: 'token',
+    phone_number: 'number'
+  }
+
+  var client = new TwilioClient(bot, config)
+
+  expect(client.config.account_sid).toEqual('sid')
+  expect(client.config.auth_token).toEqual('token')
+  expect(client.config.phone_number).toEqual('number')
+});
 
 test('should send text when sending message', () => {
 
-  var client = new TwillioClient(bot, {
+  var client = new TwilioClient(bot, {
     phone_number: '1'
   })
 
