c0b8a6017709297cd4c62c7a54e5ced6f2c4b331
James Campbell <james@supmenow.com>
2016-09-07 15:44:25 +0100

    Remove redundant send.

--- __tests__/twitter-client.js
+++ __tests__/twitter-client.js
@@ -5,8 +5,40 @@ var TwitterClient = require('../lib/twitter-client')
 
 var bot = new Bot()
 
-// function TwitterClient(config) {
-//
+test('should default to consumer key from enviromental variable', () => {
+
+  var client = new TwitterClient(bot)
+  client.send(null, 'text')
+
+  expect(client.twit.post).toBeCalledWith('statuses/update', { status: 'text' });
+});
+
+test('should use enviromental variables for key, secrets and tokens', () => {
+
+  // process.env.MESSENGER_ACCESS_TOKEN = 'access'
+  // process.env.MESSENGER_VERIFY_TOKEN = 'verify'
+  //
+  // var client = new FacebookMessengerClient(bot)
+  //
+  // expect(client.config.access_token).toEqual('access')
+  // expect(client.config.verify_token).toEqual('verify')
+  // process.env.MESSENGER_ACCESS_TOKEN = undefined
+  // process.env.MESSENGER_VERIFY_TOKEN = undefined
+});
+
+test('should use configuration object for key, secrets and tokens', () => {
+
+  // process.env.MESSENGER_ACCESS_TOKEN = 'access'
+  // process.env.MESSENGER_VERIFY_TOKEN = 'verify'
+  //
+  // var client = new FacebookMessengerClient(bot)
+  //
+  // expect(client.config.access_token).toEqual('access')
+  // expect(client.config.verify_token).toEqual('verify')
+  // process.env.MESSENGER_ACCESS_TOKEN = undefined
+  // process.env.MESSENGER_VERIFY_TOKEN = undefined
+});
+
 //   var defaults = {
 //     consumer_key: process.env.TWITTER_CONSUMER_KEY,
 //     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -16,16 +48,6 @@ var bot = new Bot()
 //
 //   this.config = Object.assign({}, defaults, config)
 //
-//   this.twit = new Twit({
-//   	consumer_key: this.config.consumer_key,
-//   	consumer_secret: this.config.consumer_secret,
-//   	access_token: this.config.access_token,
-//   	access_token_secret: this.config.access_token_secret
-//   })
-//
-//   var client = this
-//
-//   return function(bot) {
 //
 //     var stream = client.twit.stream('statuses/filter', { track: '@' + bot.name })
 //
@@ -39,13 +61,6 @@ var bot = new Bot()
 //
 //       res.success()
 //     })
-//   }
-// }
-//
-// TwitterClient.prototype.send = function(session, text) {
-//   // Reply to that tweet
-//   client.twit.post('statuses/update', { status: text }, function(err, data, response) {})
-// }
 
 test('posts status when sending message', () => {
 
