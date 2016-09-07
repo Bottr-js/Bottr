b65c93ac64f23ac2b20fb0ae88f728b67d25e92c
James Campbell <james@supmenow.com>
2016-09-07 14:44:03 +0100

    Preparing for last two client unit tests.

--- __tests__/twilio-client.js
+++ __tests__/twilio-client.js
@@ -1,3 +1,66 @@
+// var Session = require('./session')
+//
+// // Currently this client only supports texts but in the future could support calls
+// function TwilioClient(config) {
+//
+//   var defaults = {
+//     account_sid: process.env.TWILIO_ACCOUNT_SID,
+//     auth_token: process.env.TWILIO_AUTH_TOKEN,
+//     phone_number: process.env.TWILIO_PHONE_NUMBER
+//   }
+//
+//   this.config = Object.assign({}, defaults, config)
+//   this.twilio = require('twilio')(client.config.account_sid, client.config.auth_token);
+//
+//   var client = this
+//
+//   return function(bot) {
+//
+//     bot.on('webhook', function(req, res, next) {
+//
+//       // If this isn't a twillio request then carry on with other handlers
+//       if ( req.headers['user-agent'].indexOf('TwilioProxy') === -1 ) {
+//         next()
+//         return
+//       }
+//
+//       var data = Object.assign({}, req.query, req.body)
+//
+//       var message = {
+//         text: data.Body
+//       }
+//
+//       var session = new Session(data.From, {}, this)
+//
+//       bot.trigger('message_received', message, session)
+//       res.send({}) // We can't send a success code as twillio will send it
+//     })
+//   }
+// }
+//
+// TwilioClient.prototype.send = function(session, text) {
+//
+//   this.twilio.sendMessage({
+//
+//       to: meta.user,
+//       from: this.config.phone_number,
+//       body: text
+//
+//   }, function(err, responseData) {
+//
+//       if (!err) {
+//           console.log(responseData.from);
+//           console.log(responseData.body);
+//       }
+//   });
+// }
+//
+// TwilioClient.prototype.startTyping = function(session) {
+// }
+//
+// module.exports = TwilioClient
+
+
 test('adds 1 + 2 to equal 3', () => {
   expect(3).toBe(3);
 });
--- __tests__/twitter-client.js
+++ __tests__/twitter-client.js
@@ -1,3 +1,55 @@
+// var Session = require('./session')
+// var Twit = require('twit');
+//
+// function TwitterClient(config) {
+//
+//   var defaults = {
+//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     access_token: process.env.TWITTER_ACCESS_TOKEN,
+//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//   }
+//
+//   this.config = Object.assign({}, defaults, config)
+//
+//   this.twit = new Twit({
+//   	consumer_key: this.config.consumer_key,
+//   	consumer_secret: this.config.consumer_secret,
+//   	access_token: this.config.access_token,
+//   	access_token_secret: this.config.access_token_secret
+//   })
+//
+//   var client = this
+//
+//   return function(bot) {
+//
+//     var stream = client.twit.stream('statuses/filter', { track: '@' + bot.name })
+//
+//     stream.on('tweet', function (tweet) {
+//
+//       var session = new Session(tweet.user.id, {}, this)
+//
+//       bot.trigger('message_received', {
+//         text: tweet.text
+//       }, session)
+//
+//       res.success()
+//     })
+//   }
+// }
+//
+// TwitterClient.prototype.send = function(session, text) {
+//   // Reply to that tweet
+//   client.twit.post('statuses/update', { status: text }, function(err, data, response) {})
+// }
+//
+// TwitterClient.prototype.startTyping = function(session) {
+//   // Favourite tweet
+// }
+//
+// module.exports = TwitterClient
+
+
 test('adds 1 + 2 to equal 3', () => {
   expect(3).toBe(3);
 });
