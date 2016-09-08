cef415f5390be1ccbafdb0fea0919172369e0d78
James Campbell <james@supmenow.com>
2016-09-08 10:24:01 +0100

    Session fully tested.

--- __tests__/session.js
+++ __tests__/session.js
@@ -18,37 +18,23 @@ test('sets internal queue concurrency to 1', () => {
   expect(session.queue.concurrency).toEqual(1)
 });
 
-//
-// Session.prototype.send = function(text) {
-//   this.queue.push(function(cb) {
-//     session.startTyping()
-//
-//     var averageWordsPerMinute = 600
-//     var averageWordsPerSecond = averageWordsPerMinute / 60
-//     var averageWordsPerMillisecond = averageWordsPerSecond / 1000
-//     var totalWords = text.split(" ").length
-//     var typingTime = totalWords / averageWordsPerMillisecond
-//
-//     setTimeout(function() {
-//       session.client.send(session, text)
-//       cb()
-//     }, typingTime)
-//   });
-//
-//   this.queue.start()
-// }
-//
-
-
 test('should simulate typing of message', () => {
-  var session = new Session()
-  expect(clietn.startTyping).toBeCalled()
-});
-
-test('should send message via client', () => {
   var session = new Session(null, {}, client)
   session.send('text')
-  expect(client.send).toBeCalled()
+  expect(client.startTyping).toBeCalled()
+});
+
+test('should send message via client after simulating typing', (done) => {
+
+  var originalImp = client.startTyping
+  client.startTyping = jest.fn(function() {
+    done()
+  })
+
+  var session = new Session(null, {}, client)
+  session.send('text')
+
+  client.startTyping = originalImp
 });
 
 test('should tell client to simulate typing when session is told to start simulating typing', () => {
