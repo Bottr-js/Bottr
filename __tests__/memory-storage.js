cbb3c9e6c3814939ac0b45713f206e7d37259b1a
James Campbell <james@supmenow.com>
2016-09-08 15:20:31 +0100

    Preparing for new tests.

---
+++ __tests__/memory-storage.js
@@ -0,0 +1,23 @@
+it('should fetch user context', () => {
+  expect(1).toEqual(1)
+})
+
+it('should fetch conversation context', () => {
+  expect(1).toEqual(1)
+})
+
+it('should fetch account context', () => {
+  expect(1).toEqual(1)
+})
+
+it('should store user context', () => {
+  expect(1).toEqual(1)
+})
+
+it('should store conversation context', () => {
+  expect(1).toEqual(1)
+})
+
+it('should store account context', () => {
+  expect(1).toEqual(1)
+})
