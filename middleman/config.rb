ce9e6df8aa9521594eb3fcbeee62032f5a4a5706
James Campbell <james@supmenow.com>
2016-10-24 18:01:58 +0100

    Move page down.

--- middleman/config.rb
+++ middleman/config.rb
@@ -12,7 +12,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 page "/docs/*", :layout => "documentation"
-proxy "/docs.html", "/docs/installation.html"
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
@@ -20,6 +19,8 @@ proxy "/docs.html", "/docs/installation.html"
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
+
+proxy "/docs.html", "/docs/installation.html"
 
 # General configuration
 
